47b30b2abc4f2555694f26b32265a50a83b78066
AndyHan21 <hanjy@Bu.edu>
2023-12-02 20:17:15 -0500

    updated

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -6,3 +6,36 @@
 // - Run npm run test regularly to check autograding
 // - You'll need to link this file to your HTML :)
 
+document.addEventListener('DOMContentLoaded', () => {
+	const modals = document.querySelectorAll('.modal');
+	const modalContents = document.querySelectorAll('.modal-content');
+	const footers = document.querySelectorAll('.player-footer');
+
+	function openModal(modalId) {
+		document.getElementById(modalId).style.display = 'flex';
+	}
+
+
+	function closeModal() {
+		modals.forEach(modal => {
+			modal.style.display = 'none';
+		});
+	}
+
+	footers.forEach(footer => {
+		footer.addEventListener('click', function (event) {
+			event.stopPropagation();
+			const modalId = 'modal' + this.dataset.player;
+			openModal(modalId);
+		});
+	});
+
+	window.addEventListener('click', closeModal);
+
+
+	modalContents.forEach(content => {
+		content.addEventListener('click', (event) => {
+			event.stopPropagation();
+		});
+	});
+});
